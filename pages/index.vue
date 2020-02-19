27eef0f26e0721468415dafb3dd8228bcf411d2f
I-CHIEN <peterpika21@gmail.com>
2020-02-19 18:00:19 +0800

    更新

--- pages/index.vue
+++ pages/index.vue
@@ -1,61 +1,93 @@
 <template lang="pug">
-  div.d-flex(style="height:100%")
-    // 左側
-    section.bg-gray(style="width:80px;height:100%")
-
-    // 右側
-    section.flex-1.bg-primary(style="height:100%")
-      div.mx-auto.h-container.relative(style="height:100%")
+  section.flex-1.bg-primary(style="height:100%")
+    div.pt-6.mx-auto.h-container.relative(style="height:100%")
+      p.tx-gray.align-right {{fullDate}}
+      div
         div.clock {{minutes}}:{{seconds}}
-        div.d-flex.align-items-center
-          div.p-2.border-s.border-white.round-full.border-solid
-            embed(src="~assets/img/icon-bell.svg")
-          div.p-3.border-s.border-white.round-full.border-solid
-            embed(src="~assets/img/icon-play--orange.svg")
-          div.p-2.border-s.border-white.round-full.border-solid
-            embed(src="~assets/img/icon-delete.svg")
+        div.d-flex.align-items-center(style="margin-left:137px")
+          div.mr-4.p-2.svg--border
+            img(src="~assets/img/icon-bell.svg")
+          div.mr-4.p-4.svg--border.bg-gray(@click="start()")
+            img(v-if="isStart",src=`~assets/img/icon-pause--orange.svg`)
+            img(v-else,src=`~assets/img/icon-play--orange.svg`)
+          div.p-2.svg--border(@click="clear()")
+            img(src="~assets/img/icon-delete.svg")
 
-        // 底下番茄圖
-        embed.svg--bottomTomato(src="~assets/img/tomato--orange.svg")
+      // 底下番茄圖
+      embed.svg--bottomTomato(src="~assets/img/tomato--orange.svg")
 </template>
 
 <script>
 export default {
   data() {
     return {
+      isStart: false,
       setSecond: 1500, // 初始共幾秒,
       minutes: '25', // 字幕顯示(分)
-      seconds: '00' // 字幕顯示(秒)
+      seconds: '00', // 字幕顯示(秒)
+      date: new Date(),
+      interval: ''
+    }
+  },
+  computed: {
+    fullDate() {
+      let year = this.date.getFullYear()
+      let month = this.date.getMonth() + 1
+      let day = this.date.getDate()
+      let hour = this.date.getHours()
+      let min = this.date.getMinutes()
+
+      let str = `${year}.${month}.${day}  ${hour < 10 ? '0' : ''}${hour}:${min < 10 ? '0' : ''}${min}`
+      return str
     }
   },
   created() {
     let vm = this
-    let demo = window.setInterval(function() {
-      vm.counter()
+    setInterval(() => {
+      vm.date = new Date()
     }, 1000)
-
-    setTimeout(() => {
-      window.clearInterval(demo)
-    }, 5000)
   },
   methods: {
+    start() {
+      let vm = this
+      if (this.isStart) {
+        window.clearInterval(this.interval)
+      } else {
+        this.interval = window.setInterval(function() {
+          vm.counter()
+        }, 1000)
+      }
+      this.isStart = !this.isStart
+    },
+    clear() {
+      window.clearInterval(this.interval)
+      this.setSecond = 1500
+      this.minutes = '25'
+      this.seconds = '00'
+      this.isStart = false
+    },
+    // 計算時間
     counter() {
       let min = Math.floor(Math.floor(this.setSecond % 3600) / 60)
       let sec = this.setSecond % 60
-      this.setSecond -= 1
+      if (this.setSecond > 0) {
+        this.setSecond -= 1
 
-      // 分數格式調整
-      if (min < 10) {
-        this.minutes = '0' + min
-      } else {
-        this.minutes = min
-      }
+        // 分數格式調整
+        if (min < 10) {
+          this.minutes = '0' + min
+        } else {
+          this.minutes = min
+        }
 
-      // 秒數格式調整
-      if (sec < 10) {
-        this.seconds = '0' + sec
+        // 秒數格式調整
+        if (sec < 10) {
+          this.seconds = '0' + sec
+        } else {
+          this.seconds = sec
+        }
       } else {
-        this.seconds = sec
+        this.clear()
       }
     }
   }
